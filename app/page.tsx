0cba9ff41c7e06f5b8f7ead6791c8c8b839e4654
hunghg255 <giahunghust@gmail.com>
2025-12-23 15:24:06 +0700

    chore: build registry

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import * as React from "react"
+import { OpenInV0Button } from "./components/open-in-v0-button"
 
 export default function Home() {
   return (
@@ -6,11 +7,25 @@ export default function Home() {
       <header className="flex flex-col gap-1">
         <h1 className="text-3xl font-bold tracking-tight">Custom Registry</h1>
         <p className="text-muted-foreground">
-          A ahooks registry for distributing code using shadcn.
+          ahooks registry for distributing code using shadcn.
         </p>
       </header>
       <main className="flex flex-col flex-1 gap-8">
-        222
+<a href="https://ahooks.js.org/hooks/use-request/index">ahooks</a>
+
+        <div className="flex flex-col gap-4 border rounded-lg p-4 relative">
+          <div className="flex items-center justify-between">
+            <h2 className="text-sm text-muted-foreground sm:pl-3">
+              useRequest
+            </h2>
+            <OpenInV0Button name="useRequest" />
+          </div>
+          <div className="flex items-center justify-center relative">
+            <code>
+              pnpm dlx shadcn@latest add https://shadcn-ahooks.vercel.app/r/useRequest.json
+            </code>
+          </div>
+        </div>
       </main>
     </div>
   )
